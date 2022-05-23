8e5ca93f9738d37b2893231b4d650c5b7525e723
Andranik <csharp.and@hotmail.com>
2022-05-24 03:10:09 +0400

    Dynamic pattern creation.

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ languages.forEach(item => {
             "age": age
         };
 
-        let pattern = "\\[temperature]|\\[salary]|\\[name]|\\[age]";
+        let pattern = `\\[${Object.keys(token).join("]|\\[")}]`;
 
         translatedText.innerText = template.replace(new RegExp(`${pattern}`, "g"), function (match) {
             match = match.substring(1, match.length - 1);
